09434ae843c9ec72c616f4d681eea8329a05197a
kiel <1692335726@qq.com>
2025-01-25 16:49:42 +0800

    为可交易的NPC准备配置

---
+++ Source/IslandSurvival/Public/Character/ISNonePlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ISCharacterBase.h"
+#include "ISNonePlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISLANDSURVIVAL_API AISNonePlayerCharacter : public AISCharacterBase
+{
+	GENERATED_BODY()
+	
+};
